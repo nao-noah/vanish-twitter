44eb68208382a485124de820395156537efe82b2
nao-noah <naomooo.official@gmail.com>
2021-10-29 10:34:21 -0700

    change name

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,36 @@
 import { useEffect, useState } from "react";
 import type { NextPage } from "next";
-import { Tweet } from "src/components/tweet";
+import { Tweet } from "../components/tweet";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "src/styles/Home.module.css";
 
 const Home: NextPage = () => {
   const [tweets, setTweets] = useState<string[]>([]);
   const [newTweet, setNewTweet] = useState<string>("");
 
+  useEffect(() => {
+    const header = document.getElementById("header");
+    header?.addEventListener("click", vanishTweets);
+
+    return () => {
+      header?.removeEventListener("click", vanishTweets);
+    };
+  }, []);
+
+  const vanishTweets = () => {
+    window.alert("Vanish!");
+    setTweets([]);
+  };
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setTweets([...tweets, newTweet]);
+    setTweets([newTweet, ...tweets]);
     setNewTweet("");
   };
 
   return (
     <>
-      <header>Vanish Twitter</header>
+      <header id="header">Vanish Twitter</header>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
@@ -27,9 +40,11 @@ const Home: NextPage = () => {
         />
         <input type="submit" value="Tweet!" />
       </form>
-      {tweets.map((t) => (
-        <Tweet tweet={t} />
-      ))}
+      <main>
+        {tweets.map((t) => (
+          <Tweet tweet={t} />
+        ))}
+      </main>
     </>
   );
 };
